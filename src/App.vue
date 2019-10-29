ed3880e3995f31b1ea12e993ed731b04d76145b6
Matilde Aragao <matilde.aragao@grupofullsix.pt>
2019-10-29 18:29:13 +0000

    fonts

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <Nav />
     <router-view></router-view>
-   
   </div>
 </template>
 
 <script>
-import Nav from './components/Nav.vue'
+import Nav from "./components/Nav.vue";
 export default {
   name: "app",
   components: {
     Nav
   }
-}
-  
+};
 </script>
+<style>
+html {
+  font-family: "Roboto Mono", monospace;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
     <h1>About </h1>
+    <p>This is a Vue.js project using local Storage to save your favorites movies fetched from OMDb API</p>
 </div>
 </template>
 <script>
